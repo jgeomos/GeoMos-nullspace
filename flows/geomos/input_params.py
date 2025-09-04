85c901187bbfcaa5ad2417f3dbc7749b561c75da
jeremiegiraud <35801384+jeremiegiraud@users.noreply.github.com>
2025-09-04 13:39:34 +0800

    Test

--- flows/geomos/input_params.py
+++ flows/geomos/input_params.py
@@ -17,7 +17,7 @@ class InputParameters:
             value='models/model_grid.txt',
             label="Model of the area investigated for the case study"
         )
-        print(self.model_filename)
+        # print(self.model_filename)
         # self.model_filename = 'models/model_grid.txt'
         
         self.perturbation_filename = file_input(
@@ -28,20 +28,20 @@ class InputParameters:
         self.perturbation_filename = 'models/delta_m_orig.txt'
 
         # Geophysical data, e.g., Bouguer anomaly.
-        # self.data_vals_filename = file_input(
-        #     key='data_vals_filename',
-        #     value='gravity_data/data_vals.txt',
-        #     label="File containing the gravity data"
-        # )
-        self.data_vals_filename = 'gravity_data/data_vals.txt'
+        self.data_vals_filename = file_input(
+            key='data_vals_filename',
+            value='gravity_data/data_vals.txt',
+            label="File containing the gravity data"
+        )
+        # self.data_vals_filename = 'gravity_data/data_vals.txt'
 
         # Value of the background model used, e.g., in the calculation of the Bouguer anomaly.
-        # self.data_background_filename = file_input(
-        #     key='data_background_filename',
-        #     value='gravity_data/data_background.txt',
-        #     label="File containing the density response of the background model"
-        # )
-        self.data_background_filename = 'gravity_data/data_background.txt'
+        self.data_background_filename = file_input(
+            key='data_background_filename',
+            value='gravity_data/data_background.txt',
+            label="File containing the density response of the background model"
+        )
+        # self.data_background_filename = 'gravity_data/data_background.txt'
 
         sensit_files = file_input(
             key='sensit_files',
@@ -83,29 +83,29 @@ class InputParameters:
 
         print(self.sensit_path)
 
-        # self.rotation_mat_filename = file_input(
-        #     key='rotation_mat_filename', 
-        #     value='rotation_matrix.txt',
-        #     label="Path to rotation matrix",
-        #     optional=True
-        # )
-        self.rotation_mat_filename = 'rotation_matrix.txt'
+        self.rotation_mat_filename = file_input(
+            key='rotation_mat_filename', 
+            value='rotation_matrix.txt',
+            label="Path to rotation matrix",
+            optional=True
+        )
+        # self.rotation_mat_filename = 'rotation_matrix.txt'
 
-        # self.geol_model_path = file_input(
-        #     key='geol_model_path',
-        #     value='models/m_geol_orig.txt',
-        #     label="Path to geological or other reference model, used for plots only, to add a reference more for comparison",
-        #     optional=True
-        # )
-        self.geol_model_path = 'models/m_geol_orig.txt'
+        self.geol_model_path = file_input(
+            key='geol_model_path',
+            value='models/m_geol_orig.txt',
+            label="Path to geological or other reference model, used for plots only, to add a reference more for comparison",
+            optional=True
+        )
+        # self.geol_model_path = 'models/m_geol_orig.txt'
 
-        # self.data_outline_filename = file_input(
-        #     key='data_outline_filename',
-        #     value='gravity_data/ouline_core_area_dots.txt',
-        #     label="Path to file containing the outline of the geophysical data ",
-        #     optional=True
-        # )
-        self.data_outline_filename = 'gravity_data/ouline_core_area_dots.txt'
+        self.data_outline_filename = file_input(
+            key='data_outline_filename',
+            value='gravity_data/ouline_core_area_dots.txt',
+            label="Path to file containing the outline of the geophysical data ",
+            optional=True
+        )
+        # self.data_outline_filename = 'gravity_data/ouline_core_area_dots.txt'
 
         # -------------------------------
         # Section 'SolverParameters'.
